3adcfa47912f2d8b923b6db3f17d1a471004709a
Guilherme006 <137656860+Guilherme006@users.noreply.github.com>
2024-04-21 01:43:22 -0300

    Add files via upload

---
+++ primeiro-projeto-springboot/src/main/java/br/com/guilherme/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplication.java
@@ -0,0 +1,15 @@
+package br.com.guilherme.primeiroprojetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "br.com.guilherme")
+public class PrimeiroProjetoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringbootApplication.class, args);
+	}
+
+}
